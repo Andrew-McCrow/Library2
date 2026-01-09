76baf550877f3a826d3ad65c893301577156c4b1
Andrew-McCrow <andrewmccrow@gmail.com>
2026-01-09 17:30:42 +0000

    addedf modal to add new book

--- library.js
+++ library.js
@@ -1,3 +1,4 @@
+// constructor function for Book objects
 function Book(title, author, pages, read) {
   if (!new.target) {
     throw Error("You must use the 'new' operator to call the constructor");
@@ -11,22 +12,27 @@ function Book(title, author, pages, read) {
   };
 }
 
+// library array to hold Book objects
 let myLibrary = [];
 
+// Sample books
 const book1 = new Book("The Hobbit", "J.R.R. Tolkien", 310);
 const book2 = new Book("1984", "George Orwell", 328);
 const book3 = new Book("To Kill a Mockingbird", "Harper Lee", 281);
 const book4 = new Book("Pride and Prejudice", "Jane Austen", 279);
 const book5 = new Book("The Great Gatsby", "F. Scott Fitzgerald", 180);
 
+// Adding sample books to the library
 myLibrary.push(book1, book2, book3, book4, book5);
 
+// 
 function addBookToLibrary(title, author, pages, id) {
     const book = new Book(title, author, pages, id);
     myLibrary.push(book);
   return ;
 }
 
+// Function to display books in the library
 function displayBooks() {
   const bookList = document.getElementById("book-list");
 
@@ -41,7 +47,32 @@ function displayBooks() {
 // Initial display of books
 document.addEventListener("DOMContentLoaded", displayBooks);
 
+const modal = document.getElementById("new-book-modal");
+const form = document.getElementById("new-book-form");
+const cancelButton = document.getElementById("cancel-button");
+const newBookButton = document.getElementById("new-book-button");
 
+function resetForm() {
+    form.reset();
+}
 
+cancelButton.addEventListener("click", () => {
+    modal.close();
+    resetForm();
+});
 
+newBookButton.addEventListener("click", () => {
+    modal.showModal();
+});
 
+// Adding a new book
+form.addEventListener("submit", function(event) {
+    event.preventDefault();
+    const formData = new FormData(form);
+    const title = formData.get("title");
+    const author = formData.get("author");
+    const pages = formData.get("pages");
+    addBookToLibrary(title, author, pages);
+    modal.close();
+    resetForm();
+});
